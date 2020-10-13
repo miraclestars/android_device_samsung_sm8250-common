d3b5aba852d05ab2a028806495705607499d585e
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2020-10-13 21:47:38 +0900

    Use 6GB dalvik-heap config

--- common.mk
+++ common.mk
@@ -153,6 +153,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Display
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
