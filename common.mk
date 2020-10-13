6ea262f4561adfb38a181b49353f05881a2e2b0b
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2020-10-13 21:56:56 +0900

    Enable ipsec_tunnel feature
    Fixed : CTS issue due to ipsec_tunnel feature.
    
    CTS issues are showing failure on devices with first_api_level set to
    29 . With this api level certain test cases which weren't executing
    till now begin executing. Ex: android.net.cts.IpSecManagerTunnelTest.
    
    These tests mandatorily requrire ipsec_tunnel feature to be enabled.
    
    Change-Id: If52b652ac3cc6488fefcf851527f224efa03e7eb

--- common.mk
+++ common.mk
@@ -538,7 +538,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
