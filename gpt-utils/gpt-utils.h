2e7ab2f4de88c91db9e3d7cae23c859649e4bd99
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2020-11-15 20:10:10 +0900

    Align partition swap list
    Modify partition swap list to reflect only existing partitions.
    
    Bug: 70214866
    
    Change-Id: I2efe784efa97e3f2960d990a969fc729022a5ceb

--- gpt-utils/gpt-utils.h
+++ gpt-utils/gpt-utils.h
@@ -80,8 +80,12 @@ extern "C" {
 #define AB_SLOT_B_SUFFIX                "_b"
 #define PTN_XBL                         "xbl"
 #define PTN_XBL_CFG                     "xbl_config"
-#define PTN_SWAP_LIST                   PTN_XBL, PTN_XBL_CFG, "sbl1", "rpm", "tz", "aboot", "abl", "hyp", "lksecapp", "keymaster", "cmnlib", "cmnlib32", "cmnlib64", "pmic", "apdp", "devcfg", "hosd", "keystore", "msadp", "mdtp", "mdtpsecapp", "dsp", "aop", "qupfw", "vbmeta", "dtbo", "imagefv", "ImageFv", "multiimgoem", "multiimgqti", "uefisecapp"
-#define AB_PTN_LIST PTN_SWAP_LIST, "boot", "system", "vendor", "odm", "modem", "bluetooth"
+#define PTN_SWAP_LIST                   PTN_XBL, PTN_XBL_CFG, \
+            "abl", "aop", "apdp", "bksecapp", "cmnlib", "cmnlib64", "devcfg", \
+            "dtbo", "hyp", "km4", "msadp", "multi_image", "qupv3fw", "sec", "storsec", \
+            "tz", "tz_hdm", "tz_iccc", "uefi_sec", "vaultkeeper", "vbmeta", "vbmeta_system"
+#define AB_PTN_LIST PTN_SWAP_LIST, "boot", "system", "vendor", "odm", "product", "system_ext", \
+            "modem"
 #define BOOT_DEV_DIR    "/dev/block/bootdevice/by-name"
 
 /******************************************************************************
