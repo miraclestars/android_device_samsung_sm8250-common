a4e951768b2a38c7c6c6a9f2a2c0e7f8c1dcf507
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2020-11-23 17:22:43 +0900

    Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- common.mk
+++ common.mk
@@ -580,5 +580,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifi.hostapd@1.1.vendor \
     vendor.qti.hardware.wifi.supplicant@2.0.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
