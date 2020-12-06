16c5174b704e84675d7f19cdb24bf9108f333658
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2020-12-06 20:56:55 +0900

    Remove unnecessary android.hardware.radio entries
    * These are implicitly included by AOSP.
    
    Change-Id: I9e557444161e7ca60793bddde82330a6bd04908b

--- common.mk
+++ common.mk
@@ -440,9 +440,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
     android.hardware.radio@1.3-radio-service.samsung \
-    android.hardware.radio.config@1.0 \
     libprotobuf-cpp-full \
     librmnetctl \
     vendor.samsung.hardware.radio@1.2
