3302599018ee38d2bbe7ce8e9bb58eaa74c4e9d1
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2021-02-15 20:24:26 +0900

    Build Biometrics blob

--- common.mk
+++ common.mk
@@ -154,6 +154,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.face@1.0
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
