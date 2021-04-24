33f0bd71d1b9d5311b5d3fe184a856dd733a3aa9
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2021-04-24 20:41:12 +0900

    Unset NEED_KERNEL_MODULE_SYSTEM
    We are building vendor image.
    
    Change-Id: Ia7f47528c116749f897d7bcfbff6acf290a220fd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,7 +107,6 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
-NEED_KERNEL_MODULE_SYSTEM := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_ARCH := arm64
