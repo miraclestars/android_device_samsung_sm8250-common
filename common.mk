4bc19d5b24128e7bf05f6d64c350cd7b49c0e5ec
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2021-05-10 22:14:05 +0900

    usb: build usb gadget hal

--- common.mk
+++ common.mk
@@ -515,7 +515,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-qti
+    android.hardware.usb@1.2-service-qti \
+    android.hardware.usb.gadget@1.0-service-qti
 
 # Vendor libstdc++
 PRODUCT_PACKAGES += \
