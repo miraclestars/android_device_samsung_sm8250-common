8f96aaa43a8ff4f0451824a59b9b02ca2564f5bb
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2021-06-25 22:21:58 +0900

    Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- common.mk
+++ common.mk
@@ -336,6 +336,9 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
