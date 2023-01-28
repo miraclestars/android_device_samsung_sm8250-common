6a92c8a3e7fd5f6e6013fb21e4a1e86628f4f21f
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2023-01-28 23:59:47 +0900

    Delete unnecessary lines for SC52AOMU1CVK3

--- extract-files.sh
+++ extract-files.sh
@@ -63,9 +63,6 @@ function blob_fixup() {
         vendor/lib64/libsec-ril.so)
             sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
             ;;
-        vendor/lib64/libsec-ril-dsds.so)
-            sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
-            ;;
         vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so)
             # Replace libutils with vndk30 libutils
             "${PATCHELF}" --replace-needed libutils.so libutils-v30.so "${2}"
