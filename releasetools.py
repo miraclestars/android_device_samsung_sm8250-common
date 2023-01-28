ef20b5ff935324f93710e8334cbcc6a8e725876a
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2023-01-29 08:31:49 +0900

    Remove unused recovery_updater module

--- releasetools.py
+++ releasetools.py
@@ -17,29 +17,12 @@
 import common
 import re
 
-def FullOTA_Assertions(info):
-  OTA_Assertions(info)
-  return
-
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
 
-def IncrementalOTA_Assertions(info):
-  OTA_Assertions(info)
-  return
-
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
-
-def OTA_Assertions(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-bootloader-min\s*=\s*(\S+)', android_info.decode('utf-8'))
-  if m:
-    bootloader_version = m.group(1)
-    cmd = ('assert(samsung_sm8250.verify_bootloader_min("{}") == "1" || abort("ERROR: This package requires Android 13 based firmware. Please upgrade firmware and retry!"););').format(bootloader_version)
-    info.script.AppendExtra(cmd)
   return
 
 def AddImage(info, basename, dest):
