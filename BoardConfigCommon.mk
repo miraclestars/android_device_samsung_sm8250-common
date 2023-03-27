c9c557b886eb2de69ea529ce258fa01764e1774e
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2023-03-27 22:23:33 +0900

    Update based on vendor_sepolicy.cil

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -226,6 +226,7 @@ VENDOR_SECURITY_PATCH := 2023-01-01
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Wifi
