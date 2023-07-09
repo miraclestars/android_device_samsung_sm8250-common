b0758c3689e1d4426a94718c3796e16660e8165f
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2023-07-09 11:49:07 +0900

    Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- common.mk
+++ common.mk
@@ -210,7 +210,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1.vendor
+    android.hardware.health@2.1.vendor \
+    vendor.lineage.health-service.default
 
 # HIDL
 PRODUCT_PACKAGES += \
