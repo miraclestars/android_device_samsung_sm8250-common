655f5ce7f67ae96254ee60630e4674bc1a8917f0
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2023-07-09 12:05:11 +0900

    Enable fingerprint gestures
    Change-Id: I3289fac865b8d7bf5900d8fa8b3f5b03b818a230

--- common.mk
+++ common.mk
@@ -260,7 +260,8 @@ PRODUCT_COPY_FILES += \
     
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
+    $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl \
+    $(COMMON_PATH)/configs/keylayout/uinput-sec-fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-sec-fp.kl
 
 # Keymaster
 PRODUCT_PACKAGES += \
