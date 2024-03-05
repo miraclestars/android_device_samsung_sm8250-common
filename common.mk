181a2c513d0af8f8848f8272070a27f271719ea5
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2024-03-05 23:35:55 +0900

    Remove 28.0 compat support
    Treble doesn't support U system + P vendor, so removing P (28.0)
    prebuilts and compat files.
    
    Bug: 267692547
    Test: build
    Change-Id: I943a1f70b3600918a375ff62a20d45cfae2f673e

--- common.mk
+++ common.mk
@@ -535,7 +535,7 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
 
 # VNDK
-PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
+PRODUCT_EXTRA_VNDK_VERSIONS := 29 30
 
 # WiFi
 PRODUCT_PACKAGES += \
