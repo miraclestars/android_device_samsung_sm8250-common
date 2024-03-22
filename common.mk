b8c25a809eb6fdd1d3636e86a96a688c0318acc5
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2024-03-23 07:49:09 +0900

    Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- common.mk
+++ common.mk
@@ -512,10 +512,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung.sm8250
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.samsung
