619956eea2af1ab7e24dacb4393f9cd6aee07a8d
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2024-08-18 13:51:37 +0900

    Remove unneccessary keylayout
    * key 143 is already KEY_WAKEUP which is enough for dt2w
    
    Change-Id: I8c20415e3a3c104e643377a610b6aaebb4ab7956

--- common.mk
+++ common.mk
@@ -246,7 +246,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl \
     $(COMMON_PATH)/configs/keylayout/uinput-sec-fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-sec-fp.kl
 
 # Keymaster
