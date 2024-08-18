53db0dfbf7d224bf8d7b2ee2ffe2610144aaac7d
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2024-08-18 13:31:28 +0900

    Include hardware/samsung FCM
    Change-Id: Icb1a1bd6de56cbf2419cb8a046fa2a7a35cfc912

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,6 +142,7 @@ TARGET_USES_FOD_ZPOS := true
 # Framework Matrix
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/configs/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
