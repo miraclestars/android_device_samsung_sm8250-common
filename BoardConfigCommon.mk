29470eaea377072d7b2b86edb4840dd9571f7789
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2024-08-27 21:28:13 +0900

    Change SYSTEM_EXT_*_SEPOLICY_DIRS to PRODUCT_*_SEPOLICY_DIRS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -242,8 +242,8 @@ VENDOR_SECURITY_PATCH := 2024-01-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # USB
 $(call soong_config_set,samsungUsbGadgetVars,gadget_name,a600000.dwc3)
